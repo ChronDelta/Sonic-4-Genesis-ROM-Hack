1c143fad094b53c8c21643d4d99dedce76124b72
Chron Dafter <85424698+ChronDafter0@users.noreply.github.com>
2022-01-02 15:13:05 -0600

    Add files via upload

---
+++ _maps/diagonal spring.asm
@@ -0,0 +1,44 @@
+; --------------------------------------------------------------------------------
+; Sprite mappings - output from SonMapEd - Sonic 1 format
+; --------------------------------------------------------------------------------
+
+SME_j5G8K:	
+		dc.w SME_j5G8K_E-SME_j5G8K, SME_j5G8K_23-SME_j5G8K	
+		dc.w SME_j5G8K_38-SME_j5G8K, SME_j5G8K_4D-SME_j5G8K	
+		dc.w SME_j5G8K_62-SME_j5G8K, SME_j5G8K_72-SME_j5G8K	
+		dc.w SME_j5G8K_87-SME_j5G8K	
+SME_j5G8K_E:	dc.b 4	
+		dc.b $F0, $D, 0, 0, $F0	
+		dc.b 0, 5, 0, 8, 0	
+		dc.b $FB, 5, 0, $C, $F6	
+		dc.b 0, 5, $20, $1C, $F0	
+SME_j5G8K_23:	dc.b 4	
+		dc.b $E6, $D, 0, 0, $FB	
+		dc.b $F6, 5, 0, 8, $B	
+		dc.b $F3, $B, 0, $10, $F6	
+		dc.b 0, 5, $20, $1C, $F0	
+SME_j5G8K_38:	dc.b 4	
+		dc.b 0, $D, $10, 0, $F0	
+		dc.b $F0, 5, $10, 8, 0	
+		dc.b $F5, 5, $10, $C, $F6	
+		dc.b $F0, 5, $30, $1C, $F0	
+SME_j5G8K_4D:	dc.b 4	
+		dc.b $F0, $D, 0, 0, $F0	
+		dc.b 0, 5, 0, 8, 0	
+		dc.b $FB, 5, 0, $C, $F6	
+		dc.b 0, 5, 0, $1C, $F0	
+SME_j5G8K_62:	dc.b 3	
+		dc.b $F6, $D, 0, 0, $EA	
+		dc.b 6, 5, 0, 8, $FA	
+		dc.b 0, 5, 0, $1C, $F0	
+SME_j5G8K_72:	dc.b 4	
+		dc.b $E6, $D, 0, 0, $FB	
+		dc.b $F6, 5, 0, 8, $B	
+		dc.b $F3, $B, 0, $10, $F6	
+		dc.b 0, 5, 0, $1C, $F0	
+SME_j5G8K_87:	dc.b 4	
+		dc.b 0, $D, $10, 0, $F0	
+		dc.b $F0, 5, $10, 8, 0	
+		dc.b $F5, 5, $10, $C, $F6	
+		dc.b $F0, 5, $10, $1C, $F0	
+		even
---
+++ _maps/obj34.asm
@@ -0,0 +1,114 @@
+; ---------------------------------------------------------------------------
+; Sprite mappings - zone title cards
+; ---------------------------------------------------------------------------
+    	dc.w byte_C9FE-Map_obj34
+		dc.w byte_CA2C-Map_obj34
+		dc.w byte_CA5A-Map_obj34
+		dc.w byte_CA7A-Map_obj34
+		dc.w byte_CAA8-Map_obj34
+		dc.w byte_CADC-Map_obj34
+		dc.w byte_CB10-Map_obj34
+		dc.w byte_CB26-Map_obj34
+		dc.w byte_CB31-Map_obj34
+		dc.w byte_CB3C-Map_obj34
+		dc.w byte_CB47-Map_obj34
+		dc.w byte_CB8A-Map_obj34
+byte_C9FE:	dc.b 9 			; GREEN HILL
+		dc.b $F8, 5, 0,	$18, $B4
+		dc.b $F8, 5, 0,	$3A, $C4
+		dc.b $F8, 5, 0,	$10, $D4
+		dc.b $F8, 5, 0,	$10, $E4
+		dc.b $F8, 5, 0,	$2E, $F4
+		dc.b $F8, 5, 0,	$1C, $14
+		dc.b $F8, 1, 0,	$20, $24
+		dc.b $F8, 5, 0,	$26, $2C
+		dc.b $F8, 5, 0,	$26, $3C
+byte_CA2C:	dc.b 9			; LABYRINTH
+		dc.b $F8, 5, 0,	$26, $BC
+		dc.b $F8, 5, 0,	0, $CC
+		dc.b $F8, 5, 0,	4, $DC
+		dc.b $F8, 5, 0,	$4A, $EC
+		dc.b $F8, 5, 0,	$3A, $FC
+		dc.b $F8, 1, 0,	$20, $C
+		dc.b $F8, 5, 0,	$2E, $14
+		dc.b $F8, 5, 0,	$42, $24
+		dc.b $F8, 5, 0,	$1C, $34
+byte_CA5A:	dc.b 6			; MARBLE
+		dc.b $F8, 5, 0,	$2A, $CF
+		dc.b $F8, 5, 0,	0, $E0
+		dc.b $F8, 5, 0,	$3A, $F0
+		dc.b $F8, 5, 0,	4, 0
+		dc.b $F8, 5, 0,	$26, $10
+		dc.b $F8, 5, 0,	$10, $20
+		dc.b 0
+byte_CA7A:	dc.b 9			; STAR	LIGHT
+		dc.b $F8, 5, 0,	$3E, $B4
+		dc.b $F8, 5, 0,	$42, $C4
+		dc.b $F8, 5, 0,	0, $D4
+		dc.b $F8, 5, 0,	$3A, $E4
+		dc.b $F8, 5, 0,	$26, 4
+		dc.b $F8, 1, 0,	$20, $14
+		dc.b $F8, 5, 0,	$18, $1C
+		dc.b $F8, 5, 0,	$1C, $2C
+		dc.b $F8, 5, 0,	$42, $3C
+byte_CAA8:	dc.b $A			; SPRING YARD
+		dc.b $F8, 5, 0,	$3E, $AC
+		dc.b $F8, 5, 0,	$36, $BC
+		dc.b $F8, 5, 0,	$3A, $CC
+		dc.b $F8, 1, 0,	$20, $DC
+		dc.b $F8, 5, 0,	$2E, $E4
+		dc.b $F8, 5, 0,	$18, $F4
+		dc.b $F8, 5, 0,	$4A, $14
+		dc.b $F8, 5, 0,	0, $24
+		dc.b $F8, 5, 0,	$3A, $34
+		dc.b $F8, 5, 0,	$C, $44
+		dc.b 0
+byte_CADC:	dc.b $A			; SCRAP BRAIN
+		dc.b $F8, 5, 0,	$3E, $AC
+		dc.b $F8, 5, 0,	8, $BC
+		dc.b $F8, 5, 0,	$3A, $CC
+		dc.b $F8, 5, 0,	0, $DC
+		dc.b $F8, 5, 0,	$36, $EC
+		dc.b $F8, 5, 0,	4, $C
+		dc.b $F8, 5, 0,	$3A, $1C
+		dc.b $F8, 5, 0,	0, $2C
+		dc.b $F8, 1, 0,	$20, $3C
+		dc.b $F8, 5, 0,	$2E, $44
+		dc.b 0
+byte_CB10:	dc.b 4			; ZONE
+		dc.b $F8, 5, 0,	$4E, $E0
+		dc.b $F8, 5, 0,	$32, $F0
+		dc.b $F8, 5, 0,	$2E, 0
+		dc.b $F8, 5, 0,	$10, $10
+		dc.b 0
+byte_CB26:	dc.b 2			; ACT 1
+		dc.b 4,	$C, 0, $53, $EC
+		dc.b $F4, 2, 0,	$57, $C
+byte_CB31:	dc.b 2			; ACT 2
+		dc.b 4,	$C, 0, $53, $EC
+		dc.b $F4, 6, 0,	$5A, 8
+byte_CB3C:	dc.b 2			; ACT 3
+		dc.b 4,	$C, 0, $53, $EC
+		dc.b $F4, 6, 0,	$60, 8
+byte_CB47:	dc.b $D			; Oval
+		dc.b $E4, $C, 0, $70, $F4
+		dc.b $E4, 2, 0,	$74, $14
+		dc.b $EC, 4, 0,	$77, $EC
+		dc.b $F4, 5, 0,	$79, $E4
+		dc.b $14, $C, $18, $70,	$EC
+		dc.b 4,	2, $18,	$74, $E4
+		dc.b $C, 4, $18, $77, 4
+		dc.b $FC, 5, $18, $79, $C
+		dc.b $EC, 8, 0,	$7D, $FC
+		dc.b $F4, $C, 0, $7C, $F4
+		dc.b $FC, 8, 0,	$7C, $F4
+		dc.b 4,	$C, 0, $7C, $EC
+		dc.b $C, 8, 0, $7C, $EC
+		dc.b 0
+byte_CB8A:	dc.b 5			; FINAL
+		dc.b $F8, 5, 0,	$14, $DC
+		dc.b $F8, 1, 0,	$20, $EC
+		dc.b $F8, 5, 0,	$2E, $F4
+		dc.b $F8, 5, 0,	0, 4
+		dc.b $F8, 5, 0,	$26, $14
+		even
